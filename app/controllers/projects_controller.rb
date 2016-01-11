9b156306da265dc49d1f4345f58121470b3c409d
valerie <valerie.douglas@openlogic.com>
2016-01-11 16:00:07 -0700

    create new project (completes ch3) -with-1-test-error

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,33 @@
+class ProjectsController < ApplicationController
+	def index
+		@projects = Project.all
+	end
+
+	def new
+		@project = Project.new
+	#	@project.save
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+ 	def create
+        @project = Project.new(project_params)
+
+        if @project.save
+            flash[:notice] = "Project has been created."
+            redirect_to @project
+        else
+            flash[:alert] = "Project has not been created."
+            render 'new'
+        end
+   end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:name, :description)
+	end
+
+end
---
+++ spec/features/creating_projects_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+RSpec.feature "Create new project" do
+	before do 
+		visit root_path
+        click_link "New Project"
+	end
+  scenario "creating new Project" do
+    visit root_path
+    click_link "New Project"
+    fill_in "Name", with: "Sublime Text 3"
+    fill_in "Description", with: "A text editor for everyone"
+    click_button "Create Project"
+    expect(page).to have_content "Project has been created."
+
+    project = Project.find_by(name: "Sublime Text 3")
+    expect(page.current_url).to eq project_url(project)
+
+    title = "Sublime Text 3 - Projects - Ticketee"
+    expect(page).to have_title title
+  end
+end
