cf578260077bb0a93cfe7af6008d937fdbf1e575
valerie <valerie.douglas@openlogic.com>
2016-01-12 17:03:58 -0700

    redirect when project not found-completes section 4.4

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all
 	end
@@ -9,15 +11,12 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
 	end
 
 	def edit
-    @project = Project.find(params[:id])
 	end
 
 	def update
-	 @project = Project.find(params[:id])
 	  if @project.update(project_params)
 	    flash[:notice] = "Project has been updated."
 	    redirect_to @project
@@ -47,6 +46,13 @@ class ProjectsController < ApplicationController
    end
 
 	private
+
+	def set_project
+		  @project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking for could not be found."
+		redirect_to projects_path
+	end	
 
 	def project_params
 		params.require(:project).permit(:name, :description)
